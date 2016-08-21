68b1d7a0d681a94af4257c3ef5dde66410fbdcc4
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2016-08-21 23:36:02 +0300

    Added initial microservice implementation.

---
+++ Microservices/MicroService.cs
@@ -0,0 +1,31 @@
+﻿using EventCommandModelling.IOC;
+using EventCommandModelling.Transport;
+using MicroserviceModelling.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCommandModelling.Microservices
+{
+    public abstract class MicroService
+    {
+        private IPublishSubscribe _bus;
+
+        public void Initialize()
+        {
+            if (!BasicContainer.IOC.IsTypeRegistered<IPublishSubscribe>())
+            {
+                throw new ArgumentNullException("No bus transport registered.");
+            }
+
+            _bus = BasicContainer.IOC.Resolve<IPublishSubscribe>();
+        }
+
+        public void Raise(IEvent @event)
+        {
+            _bus.Publish(@event);
+        }
+    }
+}
